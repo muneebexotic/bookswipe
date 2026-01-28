00752f254576ed6a7f9bd5ae2b5f51cfbec29259
muneebexotic <muneebusman112233@gmail.com>
2026-01-28 19:16:11 +0500

    save

--- app/services/open_library.py
+++ app/services/open_library.py
@@ -56,11 +56,9 @@ async def fetch_book_from_open_library(isbn: str) -> Optional[OpenLibraryBook]:
             # Get page count
             page_count = data.get("number_of_pages")
             
-            # Get author (check edition first, then work)
+            # Get author (need to fetch from works)
             author = None
             authors_refs = data.get("authors", [])
-            
-            # Try to get author from edition level first
             if authors_refs:
                 author_key = authors_refs[0].get("key")
                 if author_key:
@@ -69,34 +67,6 @@ async def fetch_book_from_open_library(isbn: str) -> Optional[OpenLibraryBook]:
                         author_data = author_resp.json()
                         author = author_data.get("name")
             
-            # If no author found at edition level, check the work level
-            if not author:
-                works = data.get("works", [])
-                if works:
-                    work_key = works[0].get("key")
-                    if work_key:
-                        work_resp = await client.get(f"https://openlibrary.org{work_key}.json")
-                        if work_resp.status_code == 200:
-                            work_data = work_resp.json()
-                            
-                            # Get author from work
-                            work_authors = work_data.get("authors", [])
-                            if work_authors:
-                                work_author_key = work_authors[0].get("author", {}).get("key")
-                                if work_author_key:
-                                    author_resp = await client.get(f"https://openlibrary.org{work_author_key}.json")
-                                    if author_resp.status_code == 200:
-                                        author_data = author_resp.json()
-                                        author = author_data.get("name")
-                            
-                            # Also get description from work if not found at edition level
-                            if not description:
-                                desc_raw = work_data.get("description")
-                                if isinstance(desc_raw, str):
-                                    description = desc_raw
-                                elif isinstance(desc_raw, dict):
-                                    description = desc_raw.get("value")
-            
             # Get publish year
             publish_year = data.get("publish_date")
             if publish_year:
@@ -104,6 +74,21 @@ async def fetch_book_from_open_library(isbn: str) -> Optional[OpenLibraryBook]:
                 import re
                 year_match = re.search(r'\d{4}', str(publish_year))
                 publish_year = int(year_match.group()) if year_match else None
+            
+            # If no description from edition, try the work
+            if not description:
+                works = data.get("works", [])
+                if works:
+                    work_key = works[0].get("key")
+                    if work_key:
+                        work_resp = await client.get(f"https://openlibrary.org{work_key}.json")
+                        if work_resp.status_code == 200:
+                            work_data = work_resp.json()
+                            desc_raw = work_data.get("description")
+                            if isinstance(desc_raw, str):
+                                description = desc_raw
+                            elif isinstance(desc_raw, dict):
+                                description = desc_raw.get("value")
             
             return OpenLibraryBook(
                 isbn=isbn,
